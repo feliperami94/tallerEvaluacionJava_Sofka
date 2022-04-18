09829b21d41e08c835286c072aac5c2a0b5ce97c
Felipe Ramirez <felipe.ramirezrivera@gmail.com>
2022-04-18 15:48:01 -0500

    Mejoré los algoritmos de los ejercicios 5 y 6 para que CodeMetrics disminuyera la complejidad

--- src/com/tallerJava/project/ejercicio5.java
+++ src/com/tallerJava/project/ejercicio5.java
@@ -2,24 +2,23 @@ package com.tallerJava.project;
 
 public class ejercicio5 {
 
-    public static String isEven(int num){
-        String numberState = " - es impar";
-        if (num % 2 == 0) {
-            numberState = " - es par";
+    public static String isEven(int num, int numberState){
+        switch (numberState){
+            case 0:
+                return " - es par";
+            default:
+                return " - es impar";
         }
-        return numberState;
     }
 
 
     public static void main(String[] args) {
         int initialNumber = 1;
         int currentNumber = initialNumber;
-        String printingLine = "";
         int finalNumber = 100;
 
         while (currentNumber <= finalNumber ){
-            printingLine = String.valueOf(currentNumber) + isEven(currentNumber);
-            System.out.println(printingLine);
+            System.out.println(String.valueOf(currentNumber).concat(isEven(currentNumber, (currentNumber %2))));
             currentNumber++;
         }
     }
--- src/com/tallerJava/project/ejercicio6.java
+++ src/com/tallerJava/project/ejercicio6.java
@@ -2,4 +2,23 @@ package com.tallerJava.project;
 
 public class ejercicio6 {
 
+    public static String isEven(int num, int numberState){
+        switch (numberState){
+            case 0:
+                return " - es par";
+            default:
+                return " - es impar";
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int initialNumber = 1;
+        int finalNumber = 100;
+
+        for (int i = initialNumber; i <= finalNumber; i++){
+            System.out.println(String.valueOf(i).concat(isEven(i, (i % 2))));
+        }
+    }
+
 }
