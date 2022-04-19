3b196cc5bd19765cc4254f7e0bf1326f73f91cbe
Felipe Ramirez <felipe.ramirezrivera@gmail.com>
2022-04-18 19:39:36 -0500

    Hice refactor del ejercicio 11

--- src/com/tallerJava/project/ejercicio11.java
+++ src/com/tallerJava/project/ejercicio11.java
@@ -6,14 +6,7 @@ import java.util.Scanner;
 
 public class ejercicio11 {
 
-//    static
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String buffer;
-//        int phraseSize, currentCharacterCount;
-//        char currentCharacter;
-
+    static HashMap<Character, Integer> countVowels(String phrase) {
         HashMap<Character, Integer> countingVowels = new HashMap<>();
         countingVowels.put('a', 0);
         countingVowels.put('e', 0);
@@ -21,38 +14,33 @@ public class ejercicio11 {
         countingVowels.put('o', 0);
         countingVowels.put('u', 0);
 
+        for (char c: phrase.toCharArray()) {
+            switch(c){
+                case 'a':
+                case'e':
+                case'i':
+                case'o':
+                case'u':
+                countingVowels.replace(c, countingVowels.get(c) + 1);
+            }
+        }
+        return countingVowels;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String buffer;
+        int phraseSize, currentCharacterCount;
+        char currentCharacter;
+        HashMap<Character, Integer> countingVowels;
+
 
         System.out.println("Ingrese una frase para operar");
-//        buffer = input.nextLine().toLowerCase(Locale.ROOT);
-//        phraseSize = buffer.length();
-
-//        for (int i = 0; i <= (phraseSize - 1); i++) {
-//            currentCharacter = buffer.charAt(i);
-//
-//            switch (currentCharacter) {
-//                case 'a':
-//                    currentCharacterCount = countingVowels.get('a');
-//                    countingVowels.replace('a', currentCharacterCount + 1);
-//                    break;
-//                case 'e':
-//                    currentCharacterCount = countingVowels.get('e');
-//                    countingVowels.replace('e', currentCharacterCount + 1);
-//                    break;
-//                case 'i':
-//                    currentCharacterCount = countingVowels.get('i');
-//                    countingVowels.replace('i', currentCharacterCount + 1);
-//                    break;
-//                case 'o':
-//                    currentCharacterCount = countingVowels.get('o');
-//                    countingVowels.replace('o', currentCharacterCount + 1);
-//                    break;
-//                case 'u':
-//                    currentCharacterCount = countingVowels.get('u');
-//                    countingVowels.replace('u', currentCharacterCount + 1);
-//                    break;
-//            }
-//        }
+        buffer = input.nextLine().toLowerCase(Locale.ROOT);
+        phraseSize = buffer.length();
+        countingVowels = countVowels(buffer);
         System.out.println(countingVowels);
+        }
+
 
     }
-}
